ba545c09ed8bd16a8104095389ec94ebdfca1674
TJ <TJ@DESKTOP-UOO4N74>
2021-06-17 22:02:51 +0900

    My Commit

--- test/src/chapter02/MySecondClass2.java
+++ test/src/chapter02/MySecondClass2.java
@@ -7,9 +7,9 @@ public class MySecondClass2 {
 		int mynumber;
 		mynumber = 10; 
 		
-		int mynumber2 = 10;
+		int mynumber2 = 5;
 		
-		System.out.println("mynumber is "+mynumber);
-		
+		System.out.println("my number is "+mynumber);
+		System.out.println("my number is "+mynumber2);		
 	}
 }
