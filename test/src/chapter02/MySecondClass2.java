d3e55503b349052e2037f617e5beb38268321b13
TJ <TJ@DESKTOP-UOO4N74>
2021-06-17 21:58:41 +0900

    변수

--- test/src/chapter02/MySecondClass2.java
+++ test/src/chapter02/MySecondClass2.java
@@ -4,11 +4,12 @@ public class MySecondClass2 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println ("¾È³ç ÀÚ¹Ù1");
-		System.out.println ("¾È³ç ÀÚ¹Ù2");
-		System.out.println ("¾È³ç ÀÚ¹Ù3");
-		System.out.println ("¾È³ç ÀÚ¹Ù4");
-		System.out.println ("¾È³ç ÀÚ¹Ù5");
-
+		int mynumber;
+		mynumber = 10; 
+		
+		int mynumber2 = 10;
+		
+		system.out.println("mynumber is "+mynumber);
+		
 	}
 }
