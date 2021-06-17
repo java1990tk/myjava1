e785b9040d3a443dee6fc6e7c7f002a24c0abe93
TJ <TJ@DESKTOP-UOO4N74>
2021-06-17 21:45:57 +0900

    My second commit 20210617

--- test/src/chapter02/HelloJava.java
+++ test/src/chapter02/HelloJava.java
@@ -17,8 +17,8 @@ public class HelloJava {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
-		System.out.println("");
-		System.out.println("");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
 	
 
 
